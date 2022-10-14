8febc9aa03872074dc89c9ea82b695117355665c
Alexandre amorim <milton.rj30@gmail.com>
2022-10-14 15:07:25 -0300

    inital pro

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Layout } from "../components/Layout";
-import { Text } from "@chakra-ui/react";
 
 export function Home() {
 
     return (
         <Layout>
-            <Text>Home</Text>
+            <h1>Home</h1>
         </Layout>
     );
 }
